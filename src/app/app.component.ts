5cb875fbf0dd8d5571e9a7d22380f8847ca6d605
Stiven-Chacon <stivenchacon031@gmail.com>
2025-09-06 07:18:09 -0500

    feat: add Footer component with static contact info and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./core/components/navbar/navbar.component";
 import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 import { HomeComponent } from './features/home/home.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/components/footer/footer.model.ts
@@ -0,0 +1,13 @@
+export interface ContactInfo {
+  pbx: string;
+  nationalLine: string;
+  corporateLine: string;
+  address: string;
+  city: string;
+  country: string;
+}
+
+export interface FooterLink {
+  label: string;
+  href: string;
+}
